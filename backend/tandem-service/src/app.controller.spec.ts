4bc3f21459b94e7eaedc36c75d65407b406d6576
Hieu Quang Vo <hieuvoquang87@gmail.com>
2020-07-09 22:02:32 -0700

    Add Unit test

--- backend/tandem-service/src/app.controller.spec.ts
+++ backend/tandem-service/src/app.controller.spec.ts
@@ -15,8 +15,9 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Hello World!"', async () => {
+      const result = await appController.getNextDataSet();
+      expect(result.data.length).toEqual(4321);
     });
   });
 });
